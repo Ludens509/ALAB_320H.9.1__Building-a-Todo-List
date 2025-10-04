0023734056a85c15b164faa4ddeff6a607eb660a
Ismael Ludens Alexandre <alexandreludens2@gmail.com>
2025-10-04 18:42:28 -0400

    Toogle reducer

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,53 +1,70 @@
-import { useState } from "react";
+import React from "react";
 
-export default function TodoListItem({ data }) {
-  const [checkedItem, setCheckedItem] = useState(false);
+export default function TodoListItem({ state, dispatch }) {
+
+  function handleAction(e, task) {
+    const name = e.target.name;
+    if (name === "delete") {
+      dispatch({ type: "REMOVE_TODO", payload: task.id });
+    }
+    if (name === "edit") {
+      // placeholder for edit action
+      console.log('Edit', task.id);
+    }
+  }
+
+  function handleToggle(e, task) {
+    dispatch({ type: "TOGGLE_TODO", payload: task.id });
+  }
 
   return (
     <>
-      {data.map((task) => (
-        <div
-          key={task.id}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            maxWidth: "400px",
-            margin: "8px auto",
-            padding: "8px",
-            border: "1px solid #ccc",
-            borderRadius: "4px",
-          }}
-        >
-          <label>
-            <input
-              type="checkbox"
-              checked={task.completed}
-              onChange={(e) => {
-                setCheckedItem(e.target.checked);
-              }}
-            />
+      {state.map((task) => (
+        <form key={task.id} onSubmit={(e) => e.preventDefault()}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              maxWidth: "400px",
+              margin: "8px auto",
+              padding: "8px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+            }}
+          >
+            <label>
+              <input
+                type="checkbox"
+                checked={!!task.completed}
+                onChange={(e) => handleToggle(e, task)}
+              />
 
-            <span>{task.title}</span>
-          </label>
+              <span style={{ marginLeft: 8 }}>{task.title || task.text}</span>
+            </label>
 
-          <span style={{ margin: "8px" }}>
-            <button
-              disabled={checkedItem}
-              type="button"
-              className={!checkedItem ? "edit-button" : "disabled-button"}
-            >
-              {/* {task.completed ? "↺" : "✓"} */} edit
-            </button>
-            <button
-              disabled={!checkedItem}
-              type="button"
-              className={!checkedItem ? "disabled-button" : "delete-button"}
-            >
-              ✕
-            </button>
-          </span>
-        </div>
+            <span style={{ margin: "8px" }}>
+              <button
+                name="edit"
+                disabled={task.completed}
+                type="button"
+                className={!task.completed ? "edit-button" : "disabled-button"}
+                onClick={(e) => handleAction(e, task)}
+              >
+                edit
+              </button>
+              <button
+                disabled={!task.completed}
+                name="delete"
+                type="button"
+                className={!task.completed ? "disabled-button" : "delete-button"}
+                onClick={(e) => handleAction(e, task)}
+              >
+                ✕
+              </button>
+            </span>
+          </div>
+        </form>
       ))}
     </>
   );
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,39 +1,37 @@
-import { initialState } from "../utilities/data";
+import { initialState } from "../utilities/data"
 import { useReducer } from "react";
 import Form from "../components/Form";
 import Header from "../components/Header";
 import TodoListItem from "../components/TodoItem";
 
 export default function Main() {
-  // const [state, dispatch] = useReducer(reducer, initialState);
-  // console.log(state);
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "ADD_TODO":
-        return [...state, action.payload];
-      case "REMOVE_TODO":
-        return state.filter((todo) => todo.id !== action.payload);
-      case "TOGGLE_TODO":
-        return state.map((todo) =>
-          todo.id === action.payload
-            ? { ...todo, completed: !todo.completed }
-            : todo
-        );
-      default:
-        return state;
-    }
-  };
 
-  const [state, dispatch] = useReducer(reducer, initialState);
-  console.log("State", state);
+    // const [state, dispatch] = useReducer(reducer, initialState);
+    // console.log(state);
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "ADD_TODO":
+                return [...state, action.payload];
+            case "REMOVE_TODO":
+                // payload is expected to be task id
+                return state.filter((todo) => todo.id !== action.payload);
+            case "TOGGLE_TODO":
+                return state.map(todo => todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo);
+            default:
+                return state;
+        }
+    };
 
-  return (
-    <div>
-      <h1>Main Page</h1>
-      <Header />
-      <Form dispatch={dispatch} />
-      <TodoListItem state={state} dispatch={dispatch} />
-      {/* <TodoListItem state={state}  dispatch={dispatch}/> */}
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, initialState);
+    console.log("State",state); 
+
+    return (
+        <div>
+            <h1>Main Page</h1>
+            <Header/>
+            <Form dispatch={dispatch} />
+            <TodoListItem state={state} dispatch={dispatch} />
+            {/* <TodoListItem state={state}  dispatch={dispatch}/> */}
+        </div>
+    )
 }
